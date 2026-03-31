7f070524ea173f9c26935560155ae6cdb3ea40b5
Eden <eden.malhis@gmail.com>
2026-03-31 10:22:49 +0200

    restructuring the repo, adding a readme/journal and getting started on the wagtail tutorial
    Decided to test something with git, wanted to put all the content of the repo in a global directory and add other elements. Still not sure if it worked as I'm writing this but we'll see soon enough.

---
+++ README.md
@@ -0,0 +1,8 @@
+## Jour 1
+ - Tutoriel Django
+ - Comment facturer gentiment mais correctement en freelance
+ - tuples
+ - sous quelles conditions push db.sqlite dans un repo
+## Jour 2
+ - Tutoriel Wagtail
+
---
+++ Wagtail/superblog/superblog/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-#^0s%)h(ra8)h-dgf6-1a25+)00lg2mizn=56yc6*he78#=81p"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
